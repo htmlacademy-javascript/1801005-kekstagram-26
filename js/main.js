f6e71090a3bc77510f5be44aad17ce156c414216
Mangustttinio <malikov.artem@seznam.cz>
2022-06-29 23:51:53 +0200

    Расписал первую функцию иным способом

--- js/main.js
+++ js/main.js
@@ -1,18 +1,22 @@
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  let number;
-  if (min > max)
-  {
-    number = min;
-    min = max;
-    max = number;
-  };
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+//функции отвечающие за первую задачу
+//Знаю, что можно расписать в одну строку, но мне показался такой вариант неудобным
+const checkOrder = (min, max) =>
+{if (min > max) throw new Error('Перепутаны максимум и минимум')};
 
-function lengthControl(string,maxLength)
-{
-  string.length < maxLength ? console.log(true) : console.log(false);
-};
+const checkNonNegative = (min, max) =>
+{if ((min < 0)||(max < 0)) throw new Error('Значение должно быть больше нуля')};
+
+const checkInteger = (min, max) =>
+{if ((!Number.isInteger(min)) || (!Number.isInteger(min))) throw new Error ('Значение должно быть целочисленным')};
+
+const validateRange = (min, max) =>
+{if ((checkOrder(min, max) && checkNonNegative(min, max) && checkInteger(min, max)) === false) throw new Error ('Исправьте ошибку')}
+
+const createRange = (min, max) => max - min + 1;
+
+const getRandomInt = (min,max) => Math.floor(Math.random()*createRange(min,max)) + min;
+
+//вторая задача
+const checkLength = (string,maxLength) => string.length < maxLength;
+
 
